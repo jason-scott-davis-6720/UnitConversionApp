e563de1963c6c121255a1edd0ff43fac58e0c69a
Jason_Scott_Davis_6720 <94230357+JasonScottDavis@users.noreply.github.com>
2022-03-08 11:54:00 -0500

    Initial Load

---
+++ Primes/PrimeTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitConversionApp
+{
+    class PrimeTest
+    {
+
+        //public static void PrimeNumberTest(userNumber)
+        //{
+        //    int maxToCheck;
+        //    int numberToCheck;
+
+
+
+
+
+        //}
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UnitConversionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Menu.PrimeCheckMenu.MenuOneDisplay(true);
+            Menu.PrimeCheckMenu.MenuOneChoice();
+
+
+
+
+        }
+    }
+}
